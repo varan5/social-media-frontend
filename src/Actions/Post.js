7d7eb242708dc7e8e62e70099823bb0813303d1f
varan5 <varan.bhalla@gmail.com>
2022-07-21 01:22:49 +0530

    Changed backend server to development

--- src/Actions/Post.js
+++ src/Actions/Post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = process.env.REACT_APP_BACKEND_URL
+const backendUrl = process.env.REACT_APP_BACKEND_URL_DEVELOPMENT
 const token = localStorage.getItem('token')
 
 export const likePost = (id) => async (dispatch) => {
